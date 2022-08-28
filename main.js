ca9f177b1b8253f5b8ee0cdcc5827f7c3fbc1065
Chantae Palacio <chantae.palacio@gmail.com>
2022-08-28 15:00:06 -0700

    Added the javascript file

---
+++ main.js
@@ -0,0 +1,14 @@
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+
+
+let myLibrary = [];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary() {
+
+}
